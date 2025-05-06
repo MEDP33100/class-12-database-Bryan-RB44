e8e50b55bfe8d07243156fe500d94bd1a27efcf4
Bryan-RB44 <bryan.briones44@gmail.com>
2025-05-05 23:04:30 -0400

    Final Push
    Finished the assignment. Based it off the game "The Binding of Isaac".

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 [![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/sOTBciC-)
 # Template for MEDP 33100 projects and homeworks
+### Assignment
+
+Create a MongoDB database and display its contents on a page.
+
+Requirements:
+
+1. Create a new database in [MongoDB Atlas](https://cloud.mongodb.com/v2/65a9608a0842e37fef26a154#/metrics/replicaSet/6734356ece067d1843aada98/explorer/blog/posts/find)
+2. Manually create a new collection in the database
+    1. Your collection can be about anything: a collection of students, books, characters, etc.
+    2. Your collection should have at least 3 fields (ie. a collection of students has fields `name`, `grade`, `graduationYear`)
+    3. Tip: use ChatGPT to generate the data
+3. Manually add documents to your collection 
+4. Create a new Express app using `npx express-generator --view=hbs`
+5. Connect your Express app to the database in `app.js`
+6. Update `/routes/index.js` to query a collection from the database and display the data.
--- routes/index.js
+++ routes/index.js
@@ -7,29 +7,28 @@ router.get('/', async function (req, res, next) {
   try {
     //connect to posts collection.
 
-    // 1. connect to database blog
-    const blog = database.client.db('blog');
-    // 2. get collection "posts"
-    const posts = blog.collection('posts');
-    // 3. get all documents inside collection "posts" and match with collection "users"
-    const allPosts = await posts
+    // 1. connect to database tboi_game_items
+    const isaacGame = database.client.db('tboi_game_items');
+    // 2. get collection "discriptions"
+    const descriptions = isaacGame.collection('descriptions');
+    // 3. get all documents inside collection "descriptions" and match with collection "items"
+    const allItems = await descriptions
       .aggregate([
         {
           $lookup: {
-            from: 'users',
-            localField: 'authorId',
+            from: 'items',
+            localField: 'effectOwner',
             foreignField: '_id',
-            as: 'author'
+            as: 'parentItem'
           }
         }
       ])
       .toArray();
-    // console.log(allPosts);
 
     // 4. pass array to handlebars template
     res.render('index', {
-      title: 'Blog',
-      posts: allPosts,
+      title: 'Binding of Isaac Item Display DEMO',
+      descriptions: allItems,
     });
   } catch (e) {
     console.log("An error occured :/", e);
